4f942dff325f9753403dd3d6e5a74ade9433e6f8
bulan <bulan.athaillah@gmail.com>
2023-10-04 04:34:49 +0700

    feat: implement edit item feature

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, decrement_item, increment_item, delete_item
+from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, decrement_item, increment_item, delete_item, edit_item
 
 app_name = 'main'
 
@@ -16,4 +16,5 @@ urlpatterns = [
     path('decrement/<int:item_id>/', decrement_item, name='decrement_item'),
     path('increment/<int:item_id>/', increment_item, name='increment_item'),
     path('delete/<int:item_id>/', delete_item, name='delete_item'),
+    path('edit-item/<int:id>', edit_item, name='edit_item'),
 ]
