10abe50c18877ba25406618f3c3ff2640f039a01
bulan <bulan.athaillah@gmail.com>
2023-11-22 07:01:06 +0700

    fix: csrf error

--- orbit_tune/settings.py
+++ orbit_tune/settings.py
@@ -40,6 +40,7 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+CSRF_TRUSTED_ORIGINS = ['https://bulan-athaillah-tugas.pbp.cs.ui.ac.id']
 
 # Application definition
 
